4c4cbd8fadf698e958d3e63a1c4c117b229cf273
RoadRunner-2 <eva.wiedner@gmx.de>
2024-12-20 20:48:38 +0100

    implemented HW interrupt for push button, which changes the hay steamer state to 'ready'

--- HaySteamerTemperatureControl.ino
+++ HaySteamerTemperatureControl.ino
@@ -11,6 +11,8 @@ Parameter param;
 TempProbe temp(3, 7, 4, 3, 8, 4);
 U8G2_SSD1309_128X64_NONAME2_1_SW_I2C u8g2(U8G2_R0, SCL, SDA, U8X8_PIN_NONE);
 
+volatile bool push_button_was_pressed = false;
+
 void setup() {
   //Initialize serial and wait for port to open:
   Serial.begin(9600);
@@ -21,6 +23,7 @@ void setup() {
   u8g2.begin(); 
 
   pinMode(2, INPUT);
+  attachInterrupt(digitalPinToInterrupt(2), push_button_ISR, CHANGE);
 
   wlan.setup_wlan_connection();
 
@@ -33,14 +36,16 @@ void loop() {
         return;
     }
 
-  if (param.hay_steaming_status == Parameter::Status::idle && digitalRead(2) == HIGH) {
-    param.hay_steaming_status = Parameter::Status::ready;
-  }
-
   if ((millis() % 5000) == 0)
   {
     temp.update_temp();
     switch (param.hay_steaming_status) {
+      case Parameter::Status::idle:
+        if (push_button_was_pressed) {
+          push_button_was_pressed = false;
+          param.hay_steaming_status = Parameter::Status::ready;
+        }
+        break;
       case Parameter::Status::ready:
         if (time_of_day_in_minutes() >= param.start_time) {
           // switch_relay(on);
@@ -127,5 +132,10 @@ void update_display(Parameter param, int temp1, int temp2)
   } while ( u8g2.nextPage() );
 }
 
-
+void push_button_ISR()
+{
+  if (param.hay_steaming_status == Parameter::Status::idle) {
+    push_button_was_pressed = true;
+  }
+}
 
