7497ca96e87c3c64d9f8060b88a8177a1f92b6a8
RoadRunner-2 <eva.wiedner@gmx.de>
2024-12-23 11:37:00 +0100

    add error handling for states heating and holding_temperature

--- Parameter.h
+++ Parameter.h
@@ -10,7 +10,8 @@ class Parameter{
       ready,
       heating,
       holding_temperature,
-      done
+      done, 
+      error
     };
 
     void print_status()
@@ -30,18 +31,32 @@ class Parameter{
       else if (hay_steaming_status == done) {
         Serial.println("Hay Steamer Status = DONE");
       }
+      else if (hay_steaming_status == done) {
+        Serial.println("Hay Steamer Status = DONE");
+      }
+      else if (hay_steaming_status == error) {
+        Serial.println("Hay Steamer Status = ERROR");
+      }
       return;
     };
 
     Parameter(){};
 
-    Status hay_steaming_status = idle;
-    unsigned int start_time = 1007; // daily start time in minutes
-    unsigned int actual_start_time = 0; // time when the heating actually started
+    // parameter set for the steaming process
+    unsigned int start_time = 600; // daily start time in minutes
     int minimum_temperature = 30; // minimum temperature in °C
-    unsigned int reached_minimum_temperature = 0; // time of day when the minimum temperature was reached, in minutes
     unsigned int wait_time = 1; // wait time after minimum temperature was reached in minutes
+
+    // status/progress of the steaming process
+    Status hay_steaming_status = idle;
+    unsigned int actual_start_time = 0; // time when the heating actually started
+    unsigned int reached_minimum_temperature = 0; // time of day when the minimum temperature was reached, in minutes
     unsigned int time_when_done = 0; // time in minutes when the steaming process was done
+
+    // timeouts (will cause an error)
+    int heating_timeout = 2; // minimum temperature has to have been reached after this time
+    int holding_temperature_drop = 5; // temperature cannot drop more than this amount after the minimum temperature was reached
+    char error_message[20]; 
 };
 
 #endif
